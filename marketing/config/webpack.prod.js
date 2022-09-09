1883591b2fa9ef054686ed3841520909a42ba97c
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-09-09 15:54:34 +0300

    Added production webpack configs

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
+
+const domain = process.env.PROD_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap.js',
+      },
+      shared: packageJSON.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
